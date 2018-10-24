e10a21956dc9633f8f50b7b599f8ab43a8aad9af
Ahmed <barqadle999@gmail.com>
2018-10-24 11:08:20 +0300

    new file:   .gitignore 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json 	new file:   public/chat.js 	new file:   public/index.html 	new file:   public/style.css

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+var sever = app.listen(2000,"172.16.6.161",function(){
+  console.log('listening on port 2000');
+});
+
+// Static files
+app.use(express.static('public'));
+
+
+var io = socket(sever);
+
+io.on('connection',function(socket){
+  console.log('user is connect',socket.id);
+
+  socket.on("chat",function(data){
+     io.sockets.emit("chat",data);
+  });
+
+  socket.on('typing',function(data){
+    socket.broadcast.emit('typing',data)
+  });
+
+});
---
+++ public/chat.js
@@ -0,0 +1,32 @@
+// connection client
+var socket = io.connect('http://172.16.6.161:2000');
+
+//
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+var feedback= document.getElementById('feedback');
+
+    // Evvents
+
+    message.addEventListener('keypress',function(){
+      socket.emit('typing', handle.value);
+    });
+    socket.on('typing',function(data){
+      feedback.innerHTML = '<p><em>' + data + 'is typing a message.....</em></p>';
+    });
+    btn.addEventListener("click", function(){
+      socket.emit("chat", {
+        message: message.value,
+        handle: handle.value
+      });
+});
+
+
+
+
+//Listen of events
+socket.on("chat",function(data){
+  output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
